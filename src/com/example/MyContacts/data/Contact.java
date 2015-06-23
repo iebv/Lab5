5b070239ff964ea6b6b5456c3ff052dd4eb19dde
iebv <ieballesterosv@unal.edu.co>
2015-06-23 11:43:57 -0500

    Project created

---
+++ src/com/example/MyContacts/data/Contact.java
@@ -0,0 +1,51 @@
+package com.example.MyContacts.data;
+
+/**
+ * Created by fcecursos on 23/06/2015.
+ */
+public class Contact {
+
+    private String name;
+    private String email;
+    private String address;
+    private Long phone;
+
+    public Contact(String name, String email, String address, Long phone) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
